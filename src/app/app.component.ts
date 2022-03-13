c0d5008d1bd9c6b6b22f3717449506af5e881131
Madhu <madhu24321@gmail.com>
2022-03-13 12:19:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Details } from './details';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pro';
+  detail=new Details("","","","","","","","",0)
+
+  myfunc(){
+    alert("submitted");
+  }
+
+
+   
+
 }
---
+++ src/app/details.ts
@@ -0,0 +1,13 @@
+export class Details {
+    constructor(
+        public firstname:string,
+        public lastname:string,
+        public password:string,
+        public cp:string,
+        public email:string,
+        public address:string,
+        public city:string,
+        public state:string,
+        public pincode:number)
+        {}
+}
